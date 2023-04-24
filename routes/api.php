dd9ed04b309ba67fa133d60f60a92798845788f2
Paul-Iyaji <pauliyaji@gmail.com>
2023-04-24 13:49:51 +0100

    hmmn

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\AuthController;
-
-
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 
 Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group(function(){
